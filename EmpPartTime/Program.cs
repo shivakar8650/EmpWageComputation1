69d8ee712fbfe2da587c13154fe2db0e2475dff1
shivakar <shivakar@ch.iitr.ac.in>
2021-10-29 13:38:18 +0530

    [shivakar] ADD : add employee part time

--- EmpPartTime/Program.cs
+++ EmpPartTime/Program.cs
@@ -1,21 +1,26 @@
 ﻿using System;
 
-namespace Daily_Wage_Of_Employee
+namespace EmpPartTime
 {
     class Program
     {
         static void Main(string[] args)
         {
+            int IS_PART_TIME = 1;
+            int IS_FULL_TIME = 2;
 
-            int IS_FULL_TIME = 1;
             int EMP_RATE_PER_HOUR = 20;
             int empHrs = 0;
             int empWage = 0;
             Random random = new Random();
-            int empCheck = random.Next(0, 2);
+            int empCheck = random.Next(0, 3);
             if (empCheck == IS_FULL_TIME)
             {
                 empHrs = 8;
+            }
+            if (empCheck == IS_PART_TIME)
+            {
+                empHrs = 4;
             }
             else
             {
